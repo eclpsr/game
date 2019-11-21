d5685768a380628e48b70eb1790617733fb61df3
eclpsr <balupwns@gmail.com>
2019-11-21 18:22:05 +0900

    sfml lesson 6: работа со временем

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,7 +1,10 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
 using namespace sf;
 int main()
 {
+	Clock clock; // создаем переменную времени, т.о. привязка ко времени(а не загруженности/мощности процессора).
+
     RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
 
     Texture herotexture; // создаем объект Texture (текстура).
@@ -12,28 +15,37 @@ int main()
     herosprite.setTextureRect(IntRect(0,192,96,96)); //получили нужный нам прямоугольник
     herosprite.setPosition(250, 250); //выводим спрайт в позицию х у
 
+    float heroteleporttimer = 0; // создаём для примера телепортации героя через 3 секунды
+
     while (window.isOpen())
     {
+    	float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
+    	clock.restart(); //перезагружает время
+    	time = time/800; //скорость игры
+
+    	heroteleporttimer += time;
+    	if(heroteleporttimer > 3000) {herosprite.setPosition(0, 120); heroteleporttimer = 0; }
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (Keyboard::isKeyPressed(Keyboard::Left)) {
-        	herosprite.move(-0.1, 0);
+        if ((Keyboard::isKeyPressed(Keyboard::Left)) || (Keyboard::isKeyPressed(Keyboard::A)))  {
+        	herosprite.move(-0.1*time, 0);
         	herosprite.setTextureRect(IntRect(0, 96, 96, 96));
         }
-        if (Keyboard::isKeyPressed(Keyboard::Right)) {
-        	herosprite.move(0.1, 0);
+        if ((Keyboard::isKeyPressed(Keyboard::Right)) || (Keyboard::isKeyPressed(Keyboard::D))) {
+        	herosprite.move(0.1*time, 0);
         	herosprite.setTextureRect(IntRect(0, 192, 96, 96));
         }
-        if (Keyboard::isKeyPressed(Keyboard::Up)) {
-        	herosprite.move(0, -0.1);
+        if ((Keyboard::isKeyPressed(Keyboard::Up)) || (Keyboard::isKeyPressed(Keyboard::W))) {
+        	herosprite.move(0, -0.1*time);
         	herosprite.setTextureRect(IntRect(0, 288, 96, 96));
         }
-        if (Keyboard::isKeyPressed(Keyboard::Down)) {
-        	herosprite.move(0, 0.1);
+        if ((Keyboard::isKeyPressed(Keyboard::Down)) || (Keyboard::isKeyPressed(Keyboard::S))) {
+        	herosprite.move(0, 0.1*time);
         	herosprite.setTextureRect(IntRect(0, 0, 96, 96));
         }
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
