a792da91fda80d41c95c2cf91d9f675219f44961
eclpsr <balupwns@gmail.com>
2019-11-28 16:28:22 +0900

    sfml lesson 23: Работа c tiled map editor редактор карт для игр

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,21 +1,25 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
 #include <sstream>
-#include "map.h"
+// #include "map.h"
 #include "view.h"
 #include "mission.h"
 #include <cmath>
+#include<vector>
+#include<list>
+#include "level.h"
 
 using namespace sf;
 class Entity {
 public:
+	std::vector<Object> obj; // Вектор объектов карты
 	float dx, dy, x, y, speed, moveTimer;
 	int w, h, health;
 	bool life, isMove, onGround;
 	Texture texture;
 	Sprite sprite;
 	String name;
-	Entity(Image &image, float X, float Y, int W, int H, String Name){
+	Entity(Image &image, String Name, float X, float Y, int W, int H ){
 		x = X; y = Y; w = W; h = H; name = Name; moveTimer = 0;
 		speed = 0; health = 100; dx = 0; dy = 0;
 		life = true; onGround = false; isMove = false;
@@ -23,14 +27,17 @@ public:
 		sprite.setTexture(texture);
 		sprite.setOrigin(w / 2, h / 2);
 	}
+	FloatRect getRect() { //ф-ция получения прямоугольника. его коорд.размеры(шир, высот)
+		return FloatRect(x, y, w, h); // эта ф-ция нужна для проверки столкновений
+	}
 };
 
 class Player:public Entity {
 public:
 	enum {left, right, up, down, jump, stay} state; // состояние объекта
 	int playerScore;
-	Player(Image &image, float X, float Y, int W, int H, String Name):Entity(image, X, Y, W, H, Name){
-	playerScore = 0; state = stay;
+	Player(Image &image, String Name, Level &lev, float X, float Y, int W, int H ) :Entity(image, Name, X, Y, W, H ){
+	playerScore = 0; state = stay; obj = lev.GetAllObjects();
 	if (name == "Player1"){
 	sprite.setTextureRect(IntRect(4, 19, w, h));
 	}
@@ -53,7 +60,7 @@ void control(){
     }
     if ((Keyboard::isKeyPressed(Keyboard::Up)) && (onGround)){
     	state = jump;
-    	dy = -0.5;
+    	dy = -0.6;
     	onGround = false; // прыгнули и сообщили что не на земле.
     //	CurrentFrame += 0.005*time; // служит для прохождения по "кадрам".
     	//if(CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад
@@ -61,7 +68,6 @@ void control(){
     }
     if (Keyboard::isKeyPressed(Keyboard::Down)){
     	state = down;
-    	speed = 0.1;
     	// CurrentFrame += 0.005*time; // служит для прохождения по "кадрам".
     	// if(CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад
     	// p.sprite.setTextureRect(IntRect(96*int(CurrentFrame), 0, 96, 96));
@@ -69,7 +75,7 @@ void control(){
 }
 
 void checkCollisionWithMap(float Dx, float Dy) {
-	for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
+	/* for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
 	for (int j = x / 32; j<(x + w) / 32; j++)
 	{
 	if (TileMap[i][j] == '0')//если элемент наш тайлик земли? то
@@ -78,6 +84,17 @@ void checkCollisionWithMap(float Dx, float Dy) {
 	if (Dy<0){y = i * 32 + 32;  dy = 0;}//столкновение с верхними краями карты(может и не пригодиться)
 	if (Dx>0){x = j * 32 - w;}//с правым краем карты
 	if (Dx<0){x = j * 32 + 32;}// с левым краем карты
+	}
+	} */
+	for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+	if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+	{
+	if (obj[i].name == "solid"){//если встретили препятствие (объект с именем solid)
+	if (Dy>0)	{ y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+	if (Dy<0)	{ y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+	if (Dx>0)	{ x = obj[i].rect.left - w;}
+	if (Dx<0)	{ x = obj[i].rect.left + obj[i].rect.width; }
+	//}
 	}
 	}
 }
@@ -98,32 +115,33 @@ void update(float time) {
   checkCollisionWithMap(0, dy);
   sprite.setPosition(x+w/2,y+h/2);
   if (health <= 0){ life = false;}
-  if(!isMove) speed = 0;
+  if(!isMove){ speed = 0; }
   if(life){ setPlayerCoordinateForView(x, y); }
   dy = dy + 0.0015*time;
 }
 
-float getplayercoordinateX(){
+/*float getplayercoordinateX(){
 	return x;
 }
 
 float getplayercoordinateY(){
 	return y;
-}
+} */
 
 };
 
 class Enemy :public Entity{
 public:
-Enemy(Image &image, float X, float Y,int W,int H,String Name):Entity(image,X,Y,W,H,Name){
+Enemy(Image &image, String Name,Level &lvl, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H){
+obj = lvl.GetObjects("solid");//инициализируем.получаем нужные объекты для взаимодействия врага с картой
 if (name == "EasyEnemy"){
 sprite.setTextureRect(IntRect(0, 0, w, h));
-dx = 0.1;//даем скорость.этот объект всегда двигается
+dx = 0.1;
 }
 }
 void checkCollisionWithMap(float Dx, float Dy)//ф ция проверки столкновений с картой
 {
-for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
+/* for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
 for (int j = x / 32; j<(x + w) / 32; j++)
 {
 if (TileMap[i][j] == '0')//если элемент наш тайлик земли, то
@@ -133,7 +151,17 @@ if (Dy<0){ y = i * 32 + 32; }//столкновение с верхними кр
 if (Dx>0){ x = j * 32 - w; dx = -0.1; sprite.scale(-1, 1); }//с правым краем карты
 if (Dx<0){ x = j * 32 + 32; dx = 0.1; sprite.scale(-1, 1); }// с левым краем карты
 }
-}
+} */
+	for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+	if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+	{
+	//if (obj[i].name == "solid"){//если встретили препятствие (объект с именем solid)
+	if (Dy>0)	{ y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+	if (Dy<0)	{ y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+	if (Dx>0)	{ x = obj[i].rect.left - w;  dx = -0.1; sprite.scale(-1, 1); }
+	if (Dx<0)	{ x = obj[i].rect.left + obj[i].rect.width; dx = 0.1; sprite.scale(-1, 1); }
+	//}
+	}
 }
 void update(float time)
 {
@@ -147,18 +175,20 @@ if (health <= 0){ life = false; }
 }
 };
 
-int main()
-{
+int main() {
     RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
     view.reset(sf::FloatRect(0, 0, 640, 480)); // размер "вида" камеры при создании объекта вида камеры.
 
+    Level lvl; // создали экземпляр класса уровень
+    lvl.LoadFromFile("src/map.tmx");
+/*
     Image map_image; // объект изображения для карты
     map_image.loadFromFile("src/images/map.png"); // загружаем файл для карты
     Texture map; // текстура карты
     map.loadFromImage(map_image); // заряжаем текстуру картинкой
     Sprite s_map; // создаём спрайт для карты
     s_map.setTexture(map); // заливаем текстуру спрайтом
-
+*/
     Image heroImage;
     heroImage.loadFromFile("src/images/MilesTailsPrower.gif");
 
@@ -166,8 +196,10 @@ int main()
 	easyEnemyImage.loadFromFile("src/images/shamaich.png");
 	easyEnemyImage.createMaskFromColor(Color(255, 0, 0));
 
-	Player p(heroImage, 750, 500,40,30,"Player1");
-	Enemy easyEnemy(easyEnemyImage, 850, 671,200,97,"EasyEnemy");
+	Object player=lvl.GetObject("player");//объект игрока на нашей карте.задаем координаты игроку в начале при помощи него
+	Object easyEnemyObject = lvl.GetObject("easyEnemy");//объект легкого врага на нашей карте.задаем координаты игроку в начале при помощи него
+	Player p(heroImage, "Player1", lvl, player.rect.left, player.rect.top, 40, 30);//передаем координаты прямоугольника player из карты в координаты нашего игрока
+	Enemy easyEnemy(easyEnemyImage, "EasyEnemy", lvl, easyEnemyObject.rect.left, easyEnemyObject.rect.top, 200, 97);//передаем координаты прямоугольника easyEnemy из карты в координаты нашего врага
 
 	Clock clock;
     while (window.isOpen())
@@ -186,8 +218,9 @@ int main()
         p.update(time); //оживляем объект p класса Player с помощью времени sfml
         easyEnemy.update(time);
         window.setView(view); // "оживляем" камеру в окне sfml
-        window.clear();
-
+        window.clear(Color(77,83,140));
+        lvl.Draw(window); // рисуем новую карту
+      /*
         for(int i = 0; i < HEIGHT_MAP; i++)
         	for(int j = 0; j < WIDTH_MAP; j++)
         	{
@@ -198,7 +231,7 @@ int main()
         		if (TileMap[i][j] == 'h') s_map.setTextureRect(IntRect(128, 0, 32, 32));
                 s_map.setPosition(j * 32, i * 32); // по сути раскидывает квадратики, превращая в карту.
                 window.draw(s_map);
-        	}
+        	} */
         window.draw(easyEnemy.sprite);
         window.draw(p.sprite);
         window.display();
--- game/src/view.h
+++ game/src/view.h
@@ -5,13 +5,12 @@ using namespace sf;
 
 sf::View view; // объявили sfml объект "вид", который и является камерой
 
-View setPlayerCoordinateForView(float x, float y) { // ф-ция для считывания координат игрока
+void setPlayerCoordinateForView(float x, float y) { // ф-ция для считывания координат игрока
 	float tempX = x; float tempY = y; // считываем коорд игрока и проверяем их, чтобы убрать края
-	if (x < 320) tempX = 320; // убираем из вида левую сторону
-	if (y < 240) tempY = 240; // верхнюю сторону
-	if (y > 554) tempY = 554; // нижнюю сторону
+	//if (x < 320) tempX = 320; // убираем из вида левую сторону
+	//if (y < 240) tempY = 240; // верхнюю сторону
+	if (y > 720) tempY = 720; // нижнюю сторону
 	view.setCenter(tempX, tempY); // следим за игроком, передавая его координаты камере. + 100 сместили камеру иксу вправо.
-	return view;
 }
 
 
